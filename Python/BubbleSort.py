29d570811731f24a6382800a4de65ee6383a1dff
HalilKeser <hik003@post.uit.no>
2020-09-13 21:24:12 +0200

    Stasjonær

---
+++ Python/BubbleSort.py
@@ -0,0 +1,39 @@
+import random
+import timeit
+
+
+start = timeit.timeit()
+
+bubble_sorted_list = []
+
+
+def random_string(length=5):
+    consonants = "bcdfghjklmnpqrstvwxyz"
+    vowels = "aeiouæøå"
+    return "".join(random.choice((consonants, vowels)[i % 2]) for i in range(length))
+
+
+string_list = [random_string(3) for i in range(30)]
+
+print(string_list)
+
+
+def bubble_sort(string_list):
+    n = len(string_list)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if string_list[j] > string_list[j + 1]:
+                string_list[j], string_list[j + 1] = string_list[j + 1], string_list[j]
+
+
+bubble_sort(string_list)
+
+print("Sorted array is:")
+for i in range(len(string_list)):
+    bubble_sorted_list.append(string_list[i])
+print(bubble_sorted_list)
+
+end = timeit.timeit()
+print('\n', (end - start) * 1000, 'milliseconds')
+
+
