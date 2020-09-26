69edee200014ba7381e095bb42036a86c7bfb656
HalilKeser <hik003@post.uit.no>
2020-09-26 15:39:31 +0200

    Stasjonær

--- Python/ReadFile.py
+++ Python/ReadFile.py
@@ -1,6 +1,8 @@
-from collections import namedtuple
+# 1: Legg hver rad inn i en passende datastruktur,
+# feks. tuple, dict, list eller namedtuple. Skriv så ut de 5 siste radene i en celle.
+# Lager tuples med personer fra filen
+from collections import namedtuple, Counter
 
-"""Lager tuples med personer fra filen"""
 final_list = []
 personer_list = namedtuple('personer', ['etternavn', 'fornavn', 'adresse', 'postnummer', 'poststed'])
 
@@ -16,12 +18,62 @@ except FileNotFoundError as err:
 finally:
     file.close()
 
-"""Fem siste på en fin utksrift. Kunne brukt final_list[-5:]"""
-print('\n', final_list[-5], '\n', final_list[-4], '\n', final_list[-3], '\n', final_list[-2], '\n', final_list[-1],
+# Fem siste på en fin utksrift. Kunne brukt final_list[-5:]
+print('\n', *final_list[-5], '\n', *final_list[-4], '\n', *final_list[-3], '\n', *final_list[-2], '\n', *final_list[-1],
       '\n')
 
-"""Telle antall forekomster av postnummer"""
+# 2: Hvor mange unike postnummer finnes i "Personer.dta"?
+# Telle antall forekomster av postnummer
 postnummer_list = set([personer.postnummer for personer in final_list])
+print(len(postnummer_list), "\n")
 
-print(len(postnummer_list))
+# 3: Hva er de 10 vanligste etternavnene (som forekommer flest ganger) i "Personer.dta"?
+etternavn_list = Counter([personer.etternavn for personer in final_list])
+print(*etternavn_list.most_common(10))
 
+
+# 4: Implementer sorteringsalgoritmen Heap Sort og sorter listen "persons" utfra rekkefølgen radene er lest inn i.
+# Ferdig implementerte funksjoner som sorted skal ikke benyttes.
+# Skriv så ut indexene [0,20000,40000,60000,80000] fra den sorterte listen.
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    left = 2 * i + 1  # left = 2*i + 1
+    right = 2 * i + 2  # right = 2*i + 2
+
+    # See if left child of root exists and is
+    # greater than root
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+        # See if right child of root exists and is
+    # greater than root
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+        # Change root, if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+
+        # Heapify the root.
+        heapify(arr, n, largest)
+
+
+def heapSort(arr):
+    n = len(arr)
+
+    # Build a maxheap.
+    # Since last parent will be at ((n//2)-1) we can start at that location.
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+        # One by one extract elements
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # swap
+        heapify(arr, i, 0)
+
+#-------------------------------------Søk etter heapsort geek lexiograpic
+personer = [personer.etternavn for personer in personer_list]
+heapSort(personer)
+n = len(personer)
+for i in range(n):
+    print(personer[i])
