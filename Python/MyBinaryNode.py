7342e739d932f4be00196974e3c5b635738a91b2
hke006@uit.no <hke006@uit.no>
2020-09-29 19:23:52 +0200

    Bærbar

--- Python/MyBinaryNode.py
+++ Python/MyBinaryNode.py
@@ -1,3 +1,7 @@
+from __future__ import division
+import random
+
+
 class MyBinaryNode:
     def __init__(self, value, lefttree=None, righttree=None):
         self.value = value
@@ -109,17 +113,151 @@ def levelOrderEntry(self, queue):
 """leftNode = MyBinaryNode('+', MyBinaryNode('a'), MyBinaryNode('b'))
 rightNode = MyBinaryNode('-', MyBinaryNode('a'), MyBinaryNode('b'))
 rootNode = MyBinaryNode('*', leftNode, rightNode)"""
+# ----------------------------------------------------------------
+OPERATORS = set(['+', '-', '*', '/', '(', ')'])
+PRIORITY = {'+': 1, '-': 1, '*': 2, '/': 2}
 
+
+def infix_to_postfix(formula):
+    stack = []  # only pop when the coming op has priority
+    output = ''
+    for ch in formula:
+        if ch not in OPERATORS:
+            output += ch
+        elif ch == '(':
+            stack.append('(')
+        elif ch == ')':
+            while stack and stack[-1] != '(':
+                output += stack.pop()
+            stack.pop()  # pop '('
+        else:
+            while stack and stack[-1] != '(' and PRIORITY[ch] <= PRIORITY[stack[-1]]:
+                output += stack.pop()
+            stack.append(ch)
+    # leftover
+    while stack: output += stack.pop()
+    print(output)
+    return output
+
+
+def postfix_to_infix(formula):
+    stack = []
+    prev_op = None
+    for ch in formula:
+        if not ch in OPERATORS:
+            stack.append(ch)
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            if prev_op and len(a) > 1 and PRIORITY[ch] > PRIORITY[prev_op]:
+                # if previous operator has lower priority
+                # add '()' to the previous a
+                expr = '(' + a + ')' + ch + b
+            else:
+                expr = a + ch + b
+            stack.append(expr)
+            prev_op = ch
+    print(stack[-1])
+    return stack[-1]
+
+
+def infix_to_prefix(formula):
+    op_stack = []
+    exp_stack = []
+    for ch in formula:
+        if not ch in OPERATORS:
+            exp_stack.append(ch)
+        elif ch == '(':
+            op_stack.append(ch)
+        elif ch == ')':
+            while op_stack[-1] != '(':
+                op = op_stack.pop()
+                a = exp_stack.pop()
+                b = exp_stack.pop()
+                exp_stack.append(op + b + a)
+            op_stack.pop()  # pop '('
+        else:
+            while op_stack and op_stack[-1] != '(' and PRIORITY[ch] <= PRIORITY[op_stack[-1]]:
+                op = op_stack.pop()
+                a = exp_stack.pop()
+                b = exp_stack.pop()
+                exp_stack.append(op + b + a)
+            op_stack.append(ch)
+
+            # leftover
+    while op_stack:
+        op = op_stack.pop()
+        a = exp_stack.pop()
+        b = exp_stack.pop()
+        exp_stack.append(op + b + a)
+    print(exp_stack[-1])
+    return exp_stack[-1]
+
+
+def prefix_to_infix(formula):
+    stack = []
+    prev_op = None
+    for ch in reversed(formula):
+        if not ch in OPERATORS:
+            stack.append(ch)
+        else:
+            a = stack.pop()
+            b = stack.pop()
+            if prev_op and PRIORITY[prev_op] < PRIORITY[ch]:
+                exp = '(' + a + ')' + ch + b
+            else:
+                exp = a + ch + b
+            stack.append(exp)
+            prev_op = ch
+    print(stack[-1])
+    return stack[-1]
+
+
+def evaluate_postfix(formula):
+    stack = []
+    for ch in formula:
+        if ch not in OPERATORS:
+            stack.append(float(ch))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            c = {'+': a + b, '-': a - b, '*': a * b, '/': a / b}[ch]
+            stack.append(c)
+    print(stack[-1])
+    return stack[-1]
+
+
+def evaluate_infix(formula):
+    return evaluate_postfix(infix_to_postfix(formula))
+
+
+def evaluate_prefix(formula):
+    exps = list(formula)
+    while len(exps) > 1:
+        for i in range(len(exps) - 2):
+            if exps[i] in OPERATORS:
+                if not exps[i + 1] in OPERATORS and not exps[i + 2] in OPERATORS:
+                    op, a, b = exps[i:i + 3]
+                    a, b = map(float, [a, b])
+                    c = {'+': a + b, '-': a - b, '*': a * b, '/': a / b}[op]
+                    exps = exps[:i] + [c] + exps[i + 3:]
+                    break
+        print(exps)
+    return exps[-1]
+
+
+# ----------------------------------------------------------------
 if __name__ == '__main__':
     #  input("Please enter a prefix expression: ")
     str1 = "Please enter a prefix expression: "
-    stringFromInput = ['+', '+', '*', '4', '5', '6', '7']
+    infixFromInput = ('+', '+', '*', '4', '5', '6', '7')
 
-    node = MyBinaryNode(value='', lefttree='', righttree='')
+    #node = MyBinaryNode(stringFromInput[0], MyBinaryNode(stringFromInput[1]), MyBinaryNode(stringFromInput[2]))
 
-    for strings in stringFromInput:
-        node.value.setter(strings)
+    #for strings in stringFromInput:
+    #    MyBinaryNode(strings[0])
 
+    infix_to_prefix(infixFromInput)
 
 print("The infix form is: ")
 print()
