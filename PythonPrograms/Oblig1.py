c31a33a61cf373540e773a977975bbf6b33c5e7e
HalilKeser <hik003@post.uit.no>
2020-10-13 23:19:01 +0200

    Stasjonær

--- PythonPrograms/Oblig1.py
+++ PythonPrograms/Oblig1.py
@@ -1,27 +1,33 @@
 """
 Karaktersatt Obligatorisk Oppgave #1
-
-Brukte denne hjemmesiden til å implementere kode: https://www.kalmanfilter.net/alphabeta.html
-"Example 2 – Tracking the constant velocity aircraft in one dimension"
-
-Samme som i vårt eksempel er hastigheten konstant, den endrer seg ikke over tid. Det fremkommer en lineær funskjon.
-Y= aX + b
-
-Jeg bruker et alpa beta filter til oppgaven.
-
-I denne sammenheng har vi ikke en "track-to-track interval is Δt" så jeg prøver ut forskjellige verdier og prøver ut.
-Samme gjelder verdiene til alpha og beta.
-
-Mine beregnede self._xn_n_minus_1 som en predikert verdi,
-treffer ganske godt med rectangelets virkelige posisjon som er self.rect.x.
-Eksempel fra print fra consolen under:
-self.rect.x: 		 998
-self._xn_n_plus_1:  947.8297573858339
-
-Prøvde å få litt hjelp fra nettmøtene, men det var ingen hjelp å få. 13.10.20.
-Prøvde både på dagmøtet og kveldsmøtet.
-
 Halil Ibrahim Keser
+"""
+
+# Brukte denne hjemmesiden til å implementere kode: https://www.kalmanfilter.net/alphabeta.html
+# "Example 2 – Tracking the constant velocity aircraft in one dimension"
+#
+# Samme som i vårt eksempel er hastigheten konstant, den endrer seg ikke over tid. Det fremkommer en lineær funskjon.
+# Y= aX + b
+#
+# Jeg bruker et alpa beta filter til oppgaven.
+# I denne sammenheng har vi ikke en "track-to-track interval is Δt" så jeg prøver ut forskjellige verdier og prøver ut.
+# Samme gjelder verdiene til alpha og beta.
+# Jeg velger lav alpha og beta pga stor variasjon og unøyaktighet i verdier fra noise
+#
+# Prøvde å få litt hjelp fra nettmøtene, men det var ingen hjelp å få. 13.10.20.
+# Prøvde både på dagmøtet og kveldsmøtet.
+
+"""
+På en god dag resulterer dette over 70%
+Noen ganger havner den under 55. Altså 55% - 80%
+
+Eksempel fra output er nederst i siden.
+Treffer ganske godt på rosa rektangel sin posisjon og min predikerte/beregnede verdi
+self.rect.x: 		 1072        
+self._xn_n:  1080.7008569046607
+
+Ønsker gjerne løsningsforslag på oppgaven, så jeg kan implementere Pn og r på Estimate Uncertainty.
+Klarte ikke å sette opp Estimate Uncertainty selv.
 """
 
 import pygame as pg
@@ -47,7 +53,7 @@ class Projectile():
             goal = self.kalm.calc_next(goal)
 
         deltax = np.array(float(goal) - self.px)  # The track-to-track interval is Δt or Δx ?
-        # print("deltax: \t\t\t", deltax)
+        #print("deltax: \t\t\t", deltax)
         mag_delta = norm(deltax)  # * 500.0   # magnitude
         np.divide(deltax, mag_delta, deltax)
 
@@ -79,7 +85,7 @@ class Target():
 
         if self.rect.x < 300 or self.rect.x > self.background.get_width() - 300:
             self.dx *= -1
-        # print("\nself.rect.x: \t\t", self.rect.x)
+        print("\nself.rect.x: \t\t", self.rect.x)
 
     def noisy_x_pos(self):
         pos = self.rect.x
@@ -92,24 +98,24 @@ class Kalman():
     def __init__(self):
         """Initialization"""
         self._n = 0.0                # Iterasjons verdi
-        self._z_n = 0.0              # Inkommende måling av range (avstand av nois som fremkommer) fra radar
-        self._xn_n = 0.0             # Avstand i tid n=n. Jeg predikerer et tall
-        self._xn_n_minus_1 = 0.0     # Avstand i tid n=n-1
-        self._xn_n_plus_1 = 0.0      # Avstand i tid n=n+1
+        self._z_n = 1.0              # Inkommende måling av range (avstand av nois som fremkommer) fra radar
+        self._xn_n = 1.0             # Avstand i tid n=n. Jeg predikerer et tall
+        self._xn_n_minus_1 = 1.0     # Avstand i tid n=n-1
+        self._xn_n_plus_1 = 1.0      # Avstand i tid n=n+1
         """Kalman Gain"""            # Kalman gain i dette sammenheng er apha og beta
         self._Kn = 0.1
-        self._alpha = 0.1               # alpha filter
-        self._beta = 0.2              # beta filter
-        self._delta_t = 300           # The track-to-track interval Δt. 1/clock.get_fps()
+        self._alpha = 0.15              # alpha filter
+        self._beta = 0.23                # beta filter
+        self._delta_t = 280.0             # The track-to-track interval Δt. 1/clock.get_fps()
         """Velocity"""
-        self._xdotn_n = 0.0             # Velocity/hastighet i tid n=n
-        self._xdotn_n_minus_1 = 0.0     # Velocity/hastighet i tid n=n-1
-        self._xdotn_n_plus_1 = 0.0      # Velocity/hastighet i tid n=n+1
+        self._xdotn_n = 1.0             # Velocity/hastighet i tid n=n
+        self._xdotn_n_minus_1 = 1.0     # Velocity/hastighet i tid n=n-1
+        self._xdotn_n_plus_1 = 1.0      # Velocity/hastighet i tid n=n+1
         """Measurement uncertainty"""
-        self._r = 0.0
-        self._Pn_n = 0.0                # Measurement uncertainty i tid n=n
-        self._Pn_n_minus_1 = 0.0        # Measurement uncertainty i tid n=n-1
-        self._Pn_n_plus_1 = 0.0         # Measurement uncertainty i tid n=n+1
+        self._r = 1.0                   # Measurement uncertainty
+        self._Pn_n = 1.0                # Extrapolated estimate uncertainty i tid n=n
+        self._Pn_n_minus_1 = 1.0        # Extrapolated estimate uncertainty i tid n=n-1
+        self._Pn_n_plus_1 = 1.0         # Extrapolated estimate uncertainty i tid n=n+1
 
     """The Update State Equation for position"""
     def StateUpdateEquationPosition(self):
@@ -129,19 +135,22 @@ class Kalman():
         stateExtrapolationEquation = self._xn_n_plus_1
         return stateExtrapolationEquation
 
-    """Measurement uncertainty, Kalman Gain"""
-    def MeasurementUncertainty(self):
-        self._Pn_n = (1 - self._alpha) * self._Pn_n_minus_1
-        self._r = self._z_n - self._xn_n
-        self._alpha = self._Pn_n_minus_1 / (self._Pn_n_minus_1 + self._r)
+    """Estimate Uncertainty, Measurement uncertainty, Kalman Gain"""
+    # def MeasurementUncertainty(self):
+    #     self._Pn_n = (1 - self._Kn) * self._Pn_n_minus_1
+    #     self._r = self._z_n - self._xn_n
+    #     self._Kn = self._Pn_n_minus_1 / (self._Pn_n_minus_1 + self._r)
+    #     self._alpha = self._Kn
+    #     measurementUncertainty = self._Pn_n + ((self._delta_t * self._delta_t) * self._Pn_n)
+    #     return measurementUncertainty
 
-        measurementUncertainty = self._Pn_n + ((self._delta_t * self._delta_t) * self._Pn_n)
-        return measurementUncertainty
+    def Measurement(self, zi):
+        self._z_n = zi  # Setter posisjonen til target
 
     """The Main. Hovedfunksjon"""
     def calc_next(self, z_i):
         """Step 1: MEASURE, z_n fa innkommende verdi"""
-        self._z_n = z_i
+        self.Measurement(z_i)
 
         """Step 2: UPDATE, State Update, estimerer current state"""
         self._xn_n = self.StateUpdateEquationPosition()
@@ -151,17 +160,19 @@ class Kalman():
         self._xn_n_plus_1 = self.StateExtrapolationEquation()
 
         """Iteration"""
-        self._n += 1
-        self._xn_n_minus_1 = self._xn_n
-        self._xdotn_n_minus_1 = self._xdotn_n
+        self._n += 1                            # Iterer _n iterasjons teller med 1
+        self._xn_n_minus_1 = self._xn_n         # Setter forrige xn (posisjon) til nåværende xn
+        self._xdotn_n_minus_1 = self._xdotn_n   # Setter forrige x dot n (hastighet) til nåværende x dot n
+        # self._Pn_n_minus_1 = self._Pn_n
 
+        # """Oppdaterer Estimate Uncertainty"""
+        # self._Pn_n = self.MeasurementUncertainty()
 
-        # print("self._xn_n: ", self._xn_n)
-        # print("self._xn_n_plus_1: ", self._xn_n_plus_1)
-        # print("self._z_n: ", self._z_n)
-        # print("self._xdotn_n: ", self._xdotn_n)
+        print("self._xn_n: ", self._xn_n)
+        print("self._z_n: ", self._z_n)
+        print("self._xdotn_n: ", self._xdotn_n)
 
-        return self._xn_n
+        return self._xn_n  # returnerer nåværende xn verdi
 
 
 pg.init()
@@ -179,7 +190,7 @@ while running:
     target = Target(background, 32)
     missile = Projectile(background)
     k_miss = Projectile(background, Kalman())
-    last_x_pos = target.noisy_x_pos  # The radar sends a track beam in the direction of the target with a constant rate.
+    last_x_pos = target.noisy_x_pos
     noisy_draw = np.zeros((w, 20))
 
     trial = True
@@ -234,3 +245,30 @@ while running:
     print('regular score: ', round(reg_score / iters, 2), "\n")
 
 pg.quit()
+
+"""
+self.rect.x: 		 1072
+self._xn_n:  1080.7008569046607
+self._z_n:  909.9658843730549
+self._xdotn_n:  6.912647549716007
+
+self.rect.x: 		 1073
+self._xn_n:  1158.162051364084
+self._z_n:  1597.108819967483
+self._xdotn_n:  7.336839805089039
+
+self.rect.x: 		 1074
+self._xn_n:  1080.3734290502084
+self._z_n:  639.5712359382474
+self._xdotn_n:  6.910854492417816
+
+self.rect.x: 		 1075
+self._xn_n:  1012.866742639842
+self._z_n:  630.3288529810986
+self._xdotn_n:  6.541175019218191
+
+self.rect.x: 		 1076
+self._xn_n:  1019.2859362624655
+self._z_n:  1055.6613667906654
+self._xdotn_n:  6.576327746199224
+"""
